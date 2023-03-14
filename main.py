34b3d460c109fff933c41f6497f86052effa3429
Catarina D <119116333+Curlzcroft@users.noreply.github.com>
2023-03-14 22:48:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 from pprint import pprint
 
 def recipe_search(ingredient, mealType, health):
-    app_id = "c571a1e3"
-    app_key = "2a78af3ce15e5e2b04be2768fa646d2d"
+    app_id = ""
+    app_key = ""
 
     result = requests.get("https://api.edamam.com/search?q={}&app_id={}&app_key={}&mealType={}&health={}".format(ingredient, app_id, app_key, mealType, health))
 
